32e05a1110805840c33f59a06df5f200165e2587
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2022-04-29 16:36:18 +0900

    [스마트폰 게임 텀프로젝트 11]
    - 토핑 클래스 추가
    - 이미지 추가
    - 토핑 클래스 관련 멤버 변수 값 dimen에 추

---
+++ SMGP_term_project/app/src/main/java/kr/ac/tukorea/ge/sgp02/s2018182019/smgp_term_project/game/Topping.java
@@ -0,0 +1,29 @@
+package kr.ac.tukorea.ge.sgp02.s2018182019.smgp_term_project.game;
+
+import android.graphics.Canvas;
+
+import kr.ac.tukorea.ge.sgp02.s2018182019.smgp_term_project.R;
+import kr.ac.tukorea.ge.sgp02.s2018182019.smgp_term_project.framework.Sprite;
+
+public class Topping extends Sprite {
+    float x, y;
+
+    public Topping(float x, float y) {
+        super(x, y, R.dimen.topping_radius, R.mipmap.peperoni);
+    }
+
+    public void setPosition(float x,float y) {
+
+    }
+    public void update() {
+
+    }
+
+    public void draw(Canvas canvas) {
+
+    }
+
+    public void fire() {
+
+    }
+}
